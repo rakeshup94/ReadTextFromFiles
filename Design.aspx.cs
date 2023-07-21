9910541450241eb1dc875a1ad6d28030318312cf
rakesh.ardanuy <rakesh.gangwar@ardanuy.com>
2023-07-21 09:51:40 +0530

    update

--- Design.aspx.cs
+++ Design.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace WebApplication3
 {
-    public partial class WebForm1 : System.Web.UI.Page
+    public partial class Design : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
